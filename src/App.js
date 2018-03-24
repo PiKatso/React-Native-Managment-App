72fc7adee54d7b2f704f62d393ced1c31206cc7f
Kat Gifford <katgiff2007@gmail.com>
2018-03-24 14:11:23 -0700

    wire up app to recieve reducers, currently only using a dummy reducer(as required by createStore) to view app without error

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import reducers from './reducers';
+
 // create root component
 class App extends Component {
   render () {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello World!
