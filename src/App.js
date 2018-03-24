d63584f68bbef8f7a3e43555e11c382ee79a31ca
Kat Gifford <katgiff2007@gmail.com>
2018-03-24 14:39:21 -0700

    install firebase, hide API keys and import as a variable file, initialize firebase in App class component on life cycle method componentWillMount

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
+// import combinedReducers
 import reducers from './reducers';
+// import firebase config variables
+import { REACT_NATIVE_Config } from '../config.env';
 
 // create root component
 class App extends Component {
+  // add life cycle method componentWillMount to initialize firebase
+  componentWillMount() {
+    firebase.initializeApp(REACT_NATIVE_Config);
+  }
+  
   render () {
     return (
       <Provider store={createStore(reducers)}>
