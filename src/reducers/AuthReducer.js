46051e70a69b96c71351ab0f4a1e5c6a57b9c21b
Kat Gifford <katgiff2007@gmail.com>
2018-03-25 16:44:42 -0700

    add line to reducer that creates new object with all property values of state and overwrites the email property, then returns new object

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,8 +5,8 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-       
-      return state;
+      // creates new object with vaules of state, defines email prop and overwrites to object
+      return { ...state, email: action.payload };
     default:
       return state;
   }
