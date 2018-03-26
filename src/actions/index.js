388c893fab44d7dd085b8c25c2507de908144604
Kat Gifford <katgiff2007@gmail.com>
2018-03-25 18:27:24 -0700

    updated LOGIN_USER_SUCCESS to be type/global

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
+  LOGIN_USER_SUCCESS,
   PASSWORD_CHANGED
 } from './types';
 
@@ -23,7 +24,7 @@ export const loginUser = ({ email, password }) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(user => {
       dispatch({
-        type: 'LOGIN_USER_SUCCESS',
+        type: LOGIN_USER_SUCCESS,
         payload: user
       });
     });
