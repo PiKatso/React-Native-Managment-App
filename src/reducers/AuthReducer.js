6b6edaad82f1e8ed27c69f9581b36e573077da52
Kat Gifford <katgiff2007@gmail.com>
2018-03-25 17:01:16 -0700

    setup authReducer to have initial state for password and to respond to case of PASSWORD_CHANGED

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,20 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+ };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
       // creates new object with vaules of state, defines email prop and overwrites to object
       return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
     default:
       return state;
   }
