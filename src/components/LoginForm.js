cf53370f6e04e22c164fd8bc935eeafc46f33091
Kat Gifford <katgiff2007@gmail.com>
2018-03-31 11:41:35 -0700

    add helper method to watch for errors and a call to display error message when state.props.error exists

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,8 +18,18 @@ class LoginForm extends Component {
 
   onButtonPress() {
     const { email, password } = this.props;
-    
+
     this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+        </View>
+      )
+    }
   }
 
   render() {
@@ -44,6 +54,8 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        {this.renderError()}
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -54,10 +66,19 @@ class LoginForm extends Component {
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
-    password: state.auth.password
+    password: state.auth.password,
+    error: state.auth.error
   };
 };
 
