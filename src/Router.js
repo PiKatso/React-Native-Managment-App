a1150080026f80ec432bf863c2bbbce6995d4d03
Kat Gifford <katgiff2007@gmail.com>
2018-03-31 13:23:43 -0700

    add first child scene/view to display LoginComponent

--- src/Router.js
+++ src/Router.js
@@ -6,8 +6,8 @@ import LoginForm from './components/LoginForm';
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root"> 
-
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="Login" />
       </Scene>
     </Router>
   );
