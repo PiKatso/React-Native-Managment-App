cda8aa05b2c743b8d5f5d25a00c2dbd61178adec
Kat Gifford <katgiff2007@gmail.com>
2018-03-31 13:21:36 -0700

    create Router file and set component with library, Router and root Scene/View

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene key="root"> 
+
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;
