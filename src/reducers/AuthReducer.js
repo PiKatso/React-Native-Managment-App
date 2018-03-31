b43f33850966ec0c7e7499179581bd17df31896a
Kat Gifford <katgiff2007@gmail.com>
2018-03-31 12:19:43 -0700

    add case LOGIN_USER to auth reducer and add loading prop & updates to prop by case

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,5 +1,6 @@
 import {
   EMAIL_CHANGED,
+  LOGIN_USER,
   LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
   PASSWORD_CHANGED
@@ -10,7 +11,8 @@ const INITIAL_STATE = {
   email: '',
   password: '',
   user: null,
-  error: ''
+  error: '',
+  loading: false
  };
 
 export default (state = INITIAL_STATE, action) => {
@@ -22,10 +24,12 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: '' };
+      return { ...state, user: action.payload, loading: false, error: '' };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed', password: '' };
+      return { ...state, error: 'Authentication Failed', password: '', loading: false };
     default:
       return state;
   }
