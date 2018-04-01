c71be43ea4d79e278d1323a4d92dbde9684fff2f
Kat Gifford <katgiff2007@gmail.com>
2018-03-31 18:08:52 -0700

    create EmployeeList component with generic code for testing route

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Potential EmployeeList</Text>
+        <Text>Potential EmployeeList</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList
