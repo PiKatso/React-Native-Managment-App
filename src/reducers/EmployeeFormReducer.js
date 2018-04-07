f1a4ffe4ade4e834024e6198ebbfc7e4cb075c23
Kat Gifford <katgiff2007@gmail.com>
2018-04-07 13:30:37 -0700

    create EmployeeUpdateForm reducer to set initial state and contain default state

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,12 @@
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITIALIZE_STATE = {};
+
+export default (state = INITIALIZE_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
