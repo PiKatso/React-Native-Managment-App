1aa32c7a2e74aaf31eb7a3c98970b3f215ae6e15
Kat Gifford <katgiff2007@gmail.com>
2018-04-07 13:38:24 -0700

    complete the first case for employeeupdate action to take any property and update with given value and initial state

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,18 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITIALIZE_STATE = {};
+const INITIALIZE_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIALIZE_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name, value: jane' }
+      //  '[action.payload.prop]' is key interpolation to set the key prop
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }
