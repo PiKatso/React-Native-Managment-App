df6097c5f4959c3f8ea584205027e515126d3dfc
Kat Gifford <katgiff2007@gmail.com>
2018-04-08 12:41:28 -0700

    add ListView  to make use of the async action creator call and try to make use of the data

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
+    // tells action creator to fetch list of employees || async request
     this.props.employeesFetch();
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
   }
 
   render() {
