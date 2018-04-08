a8115bbea300ca7da9f9761194297728092916f7
Kat Gifford <katgiff2007@gmail.com>
2018-04-08 14:08:46 -0700

    add a route/touchable link on emplyee row

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.employeeCreate();
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <View>
-        <CardSection>
-          <Text style={styles.titleStyle}>
-            {name}
-          </Text>
-        </CardSection>
-      </View>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
