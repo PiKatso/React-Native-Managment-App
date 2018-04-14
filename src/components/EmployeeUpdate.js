1ea9168b8e0dd144c2208c83bc069585e56b75ea
Kat Gifford <katgiff2007@gmail.com>
2018-04-14 13:40:30 -0700

    update the emplyeeUpdate component to take the employee prps and send them to the reducer

--- src/components/EmployeeUpdate.js
+++ src/components/EmployeeUpdate.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
+import { employeeUpdate } from '../actions';
 import EmployeeForm from './EmployeeForm';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeUpdate extends Component {
+  componentWillMount() {
+    // takes employee model coming in and prop iterates over it, updateing the reducer withthe props
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeUpdate({ prop, value });
+    });
+  }
+
   render() {
     return (
       <Card>
@@ -20,4 +29,4 @@ class EmployeeUpdate extends Component {
   }
 }
 
-export default connect()(EmployeeUpdate);
+export default connect(null, { employeeUpdate })(EmployeeUpdate);
