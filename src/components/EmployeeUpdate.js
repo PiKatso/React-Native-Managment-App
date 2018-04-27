e332c1a3f5bd7a908074b973c2635534faa57de0
Kat Gifford <katgiff2007@gmail.com>
2018-04-26 19:26:17 -0700

    add react-native-communications library and begin to add the text shift function to the EmployeeUpdate component

--- src/components/EmployeeUpdate.js
+++ src/components/EmployeeUpdate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import Communications from 'react-native-communications';
 
 import { employeeUpdate, employeeSave } from '../actions';
 import EmployeeForm from './EmployeeForm';
@@ -19,6 +20,12 @@ class EmployeeUpdate extends Component {
 
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
+  
+  onTextPress() {
+    const { phone, shift } = this.props;
+    
+    Communications.text(phone, `your next shift is on ${shift}`);
+  }
 
   render() {
     return (
@@ -29,6 +36,10 @@ class EmployeeUpdate extends Component {
           <Button onPress={this.onButtonPress.bind(this)}>
             Update
           </Button>
+        </CardSection>
+        
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}
         </CardSection>
       </Card>
     );
